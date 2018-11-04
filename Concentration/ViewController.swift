400a31b01b4da15ad8399d03a6e1987a0a9a3424
Rodolfo Rodriguez <rodolfo.rodrigarci@gmail.com>
2018-11-04 10:37:21 -0600

    Initial Commit

---
+++ Concentration/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Concentration
+//
+//  Created by Rodolfo Rodriguez on 11/4/18.
+//  Copyright © 2018 Rodolfo Rodriguez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
